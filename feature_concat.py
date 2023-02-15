f2a1981de37cfacc431fc5de25a0dc79f757c9a8
Ruoyu Zhang <ruoyuzha@andrew.cmu.edu>
2023-02-15 00:15:23 -0500

    Refactor pipeline

--- feature_concat.py
+++ feature_concat.py
@@ -3,11 +3,16 @@ import json
 if __name__ == "__main__":
     index  = 0
     dict_output = {}
+    # load from metadata feature
     dict_metadata = json.load("./output/final_data.json".read())
+    # load from transcription feature, which only includes transcription confidence
     dict_transcription = json.load("./output/transcription_feature.json".read())
+    # load from video feature, 3 features
     dict_video = json.load("./output/video_feature.json".read())
+    # load from text feature, 14 features
+
     # extract output features
     dict_output['views'] = dict_metadata[index]['views']
     dict_output['channel_subscribers'] = dict_metadata[index]['durations']
     
-    
+    # 
---
+++ nlp/text_analytics.py
@@ -0,0 +1,17 @@
+# transcription word count
+# transcription unique word count
+# transcription sentence count
+# transcription active word count
+# transcription summary word count
+# description word count
+# description unique word count
+# description sentence count
+# description active word count
+# description summary word count
+# description ARI
+# medical_entity_count
+# description ARI
+# medical_entity_count
+def medical_entity_count(text):
+    
+    return num_of_entity
